e5d18eabe5cceb82d96bc0e8899a2df13bdd1b52
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-03-04 12:27:07 +0530

    learnt basics

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
+import { Component,  } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+
+  pressingButton = () => {
+    alert('called function 2')
+  }
+
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Rishabh's clone</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <Text>Welcome to New Text</Text>
+      <Text style={{color: 'black'}}>Another Text</Text>
+      <Button title='Helo'/>
+      <TouchableOpacity style={
+        {height: 50, 
+        paddingHorizontal: 10, 
+        backgroundColor:'red', 
+        justifyContent: 'center',
+        alignItems:'center'}}
+        
+        onPress={() => this.pressingButton()}
+        >
+          <Text style={{fontSize:25}}>Button 2</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
